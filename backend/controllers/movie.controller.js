f270ae760e2857e1fe4d804bb7bd70eb7e80378f
Bodzillaa <bodruddoza.araf@g.bracu.ac.bd>
2024-12-25 17:27:24 +0600

    Add Home and specific movie Details functionality

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -23,3 +23,44 @@ export async function getTrendingMovie(req, res) {
     res.status(500).send("Server Error");
   }
 }
+
+export async function getAllMovies(req, res) {
+  try {
+    if (!connection) {
+      connection = await connectDB();
+    }
+
+    const [movies] = await connection.query("SELECT * FROM movies");
+
+    if (movies.length === 0) {
+      return res.status(404).json({ success: false, msg: "No movies found" });
+    }
+
+    res.status(200).json({ success: true, content: movies });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+}
+
+export async function getMovieDetails(req, res) {
+  const { id } = req.params;
+
+  try {
+    if (!connection) {
+      connection = await connectDB();
+    }
+
+    const [movie] = await connection.query(
+      "SELECT * FROM movies WHERE movie_id = ?",
+      [id]
+    );
+
+    res.status(200).json({ success: true, content: movie[0] });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    res.status(500).send("Server Error");
+  }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getTrendingMovie } from "../controllers/movie.controller.js";
+import {
+  getAllMovies,
+  getMovieDetails,
+  getTrendingMovie,
+} from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
+router.get("/", getAllMovies);
+router.get("/:id/details", getMovieDetails);
 
 export default router;

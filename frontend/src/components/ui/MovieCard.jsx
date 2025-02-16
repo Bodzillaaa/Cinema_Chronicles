d6a1254f594757cc317862b23ede344a9241e173
Bodzillaa <bodruddoza.araf@g.bracu.ac.bd>
2025-02-16 11:47:31 +0600

    Some UI optimisations

--- frontend/src/components/ui/MovieCard.jsx
+++ frontend/src/components/ui/MovieCard.jsx
@@ -6,57 +6,57 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const MovieCard = ({ movie }) => {
-  const buttonBg = useColorModeValue("black", "white");
-  const buttonColor = useColorModeValue("black", "white");
+    const buttonBg = useColorModeValue("black", "white");
+    const buttonColor = useColorModeValue("black", "white");
 
-  const addToWatchlist = async () => {
-    try {
-      const res = await axios.post(`/api/user/watchlist/${movie.movie_id}`);
-      if (!res.data.success) {
-        toast.error("Movie added to watchlist.");
-      } else {
-        // alert("Failed to add movie to watchlist.");
-        toast.success(res.data.message);
-      }
-    } catch (error) {
-      console.error("Error adding movie to watchlist", error);
-      // alert("Failed to add movie to watchlist.");
-      toast.error("Failed to add movie to watchlist.");
-    }
-  };
+    const addToWatchlist = async () => {
+        try {
+            const res = await axios.post(
+                `/api/user/watchlist/${movie.movie_id}`
+            );
+            if (!res.data.success) {
+                toast.error("Movie added to watchlist.");
+            } else {
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.error("Error adding movie to watchlist", error);
+            toast.error("Failed to add movie to watchlist.");
+        }
+    };
 
-  return (
-    <>
-      <Card.Root h={"xl"} shadow={"xl"}>
-        <Image
-          src={movie.posterUrl || "/download.jpg"}
-          alt={`${movie.title} poster`}
-        />
-        <Card.Body gap="2">
-          <Card.Title>{movie.title || "title"}</Card.Title>
-          <Card.Description>
-            {movie.description ||
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
-          </Card.Description>
-          <Text textStyle="xs" mt="2">
-            Duration: {movie.duration}
-          </Text>
-        </Card.Body>
-        <Card.Footer gap="2">
-          <Button
-            color={buttonColor}
-            variant="outline"
-            onClick={addToWatchlist}
-          >
-            Add to watchlist
-          </Button>
-          <Link to={`/details/${movie.movie_id}`}>
-            <Button bg={buttonBg}>Get Details</Button>
-          </Link>
-        </Card.Footer>
-      </Card.Root>
-    </>
-  );
+    return (
+        <>
+            <Card.Root h={"xl"} shadow={"xl"}>
+                <Image
+                    src={movie.posterUrl || "/download.jpg"}
+                    alt={`${movie.title} poster`}
+                />
+                <Card.Body gap='2'>
+                    <Card.Title>{movie.title || "title"}</Card.Title>
+                    <Card.Description>
+                        {movie.description ||
+                            "Description Loading. Please wait a moment..."}
+                    </Card.Description>
+                    <Text textStyle='xs' mt='2'>
+                        Duration: {movie.duration}
+                    </Text>
+                </Card.Body>
+                <Card.Footer gap='2'>
+                    <Button
+                        color={buttonColor}
+                        variant='outline'
+                        onClick={addToWatchlist}
+                    >
+                        Add to watchlist
+                    </Button>
+                    <Link to={`/details/${movie.movie_id}`}>
+                        <Button bg={buttonBg}>Get Details</Button>
+                    </Link>
+                </Card.Footer>
+            </Card.Root>
+        </>
+    );
 };
 
 export default MovieCard;
--- frontend/src/components/ui/Navbar.jsx
+++ frontend/src/components/ui/Navbar.jsx
@@ -8,76 +8,74 @@ import { useAuthUser } from "../../store/authUser";
 import CCLogo from "./cc_logo";
 
 const Navbar = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
+    const { colorMode, toggleColorMode } = useColorMode();
 
-  const bgColor = colorMode === "light" ? "white" : "black";
-  const textColor = colorMode === "light" ? "black" : "cyan";
+    const bgColor = colorMode === "light" ? "white" : "black";
+    const textColor = colorMode === "light" ? "black" : "cyan";
 
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const { logout } = useAuthUser();
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const { logout } = useAuthUser();
 
-  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+    const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
-  return (
-    <header>
-      <Box bg={bgColor} color={textColor}>
-        <Container p={{ base: 4, md: 8 }}>
-          <Flex justify="space-between" align="center">
-            <Link to="/">
-              {/* <Image
-                src="finallyfinal.png"
-                alt="Cinema Chronicles"
-                w={{ base: "100px", md: "150px" }}
-              /> */}
-              <CCLogo />
-            </Link>
-            <Stack
-              direction={["column", "row"]}
-              gap={4}
-              align="center"
-              display={{ base: "none", md: "flex" }}
-            >
-              <Link to="/trending">Random Movie</Link>
-              <Link to="/watchlist">Watchlist</Link>
-              <Link to="/:query">
-                <Search />
-              </Link>
+    return (
+        <header>
+            <Box bg={bgColor} color={textColor}>
+                <Container p={{ base: 4, md: 8 }}>
+                    <Flex justify='space-between' align='center'>
+                        <Link to='/'>
+                            <CCLogo />
+                        </Link>
+                        <Stack
+                            direction={["column", "row"]}
+                            gap={4}
+                            align='center'
+                            display={{ base: "none", md: "flex" }}
+                        >
+                            <Link to='/trending'>Random Movie</Link>
+                            <Link to='/watchlist'>Watchlist</Link>
+                            <Link to='/:query'>
+                                <Search />
+                            </Link>
 
-              <Button onClick={toggleColorMode}>
-                {colorMode === "light" ? <Moon /> : <Sun />}
-              </Button>
+                            <Button onClick={toggleColorMode}>
+                                {colorMode === "light" ? <Moon /> : <Sun />}
+                            </Button>
 
-              <LogOut onClick={logout} cursor={"pointer"} />
-            </Stack>
-            <Box display={{ base: "block", md: "none" }}>
-              <Menu onClick={toggleMobileMenu} cursor={"pointer"} />
+                            <LogOut onClick={logout} cursor={"pointer"} />
+                        </Stack>
+                        <Box display={{ base: "block", md: "none" }}>
+                            <Menu
+                                onClick={toggleMobileMenu}
+                                cursor={"pointer"}
+                            />
+                        </Box>
+                    </Flex>
+                    {isMobileMenuOpen && (
+                        <Stack
+                            direction='column'
+                            gap={4}
+                            align='left'
+                            display={{ base: "flex", md: "none" }}
+                            mt={4}
+                        >
+                            <Link to='/trending'>Random Movie</Link>
+                            <Link to='/watchlist'>Watchlist</Link>
+                            <Link to='/:query'>
+                                <Search />
+                            </Link>
+
+                            <Button onClick={toggleColorMode} w={"fit-content"}>
+                                {colorMode === "light" ? <Moon /> : <Sun />}
+                            </Button>
+
+                            <LogOut onClick={logout} cursor={"pointer"} />
+                        </Stack>
+                    )}
+                </Container>
             </Box>
-          </Flex>
-          {isMobileMenuOpen && (
-            <Stack
-              direction="column"
-              gap={4}
-              align="left"
-              display={{ base: "flex", md: "none" }}
-              mt={4}
-            >
-              <Link to="/trending">Random Movie</Link>
-              <Link to="/watchlist">Watchlist</Link>
-              <Link to="/:query">
-                <Search />
-              </Link>
-
-              <Button onClick={toggleColorMode} w={"fit-content"}>
-                {colorMode === "light" ? <Moon /> : <Sun />}
-              </Button>
-
-              <LogOut onClick={logout} cursor={"pointer"} />
-            </Stack>
-          )}
-        </Container>
-      </Box>
-    </header>
-  );
+        </header>
+    );
 };
 
 export default Navbar;
